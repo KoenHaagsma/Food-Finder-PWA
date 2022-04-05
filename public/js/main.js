1db2670ded83f341101e7c24540f5e1d44a5990d
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-04-05 11:19:07 +0200

    Add changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,18 @@
+import { detect } from './detect.js';
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
         navigator.serviceWorker.register('/js/service-worker.js').then(function (registration) {
             return registration.update();
         });
+    });
+}
+
+if (window.location.pathname === '/scanner') {
+    const buttons = document.querySelectorAll('button');
+    const div = document.querySelector('.container');
+    const url = 'https://world.openfoodfacts.org/api/v0/product/';
+    detect.start(`.${div.className}`, (result) => {
+        console.log(result);
     });
 }
