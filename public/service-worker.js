7609a19c15bddeb8fbf6ee7c96a633fd793a5027
Koen Haagsma <koenhaagsma123@hotmail.nl>
2022-04-06 16:28:24 +0200

    Add manifest logo's and readme images

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,18 @@
-const CORE_CACHE_VERSION = 'core-v3.3.3';
-const DYNAMIC_CACHE_VERSION = 'dynamic-v3.3.3';
-const CACHE_FILES = ['/css/style.min.css', '/manifest.json', '/js/main.min.js', '/offline'];
+const CORE_CACHE_VERSION = 'core-v3.3.4';
+const DYNAMIC_CACHE_VERSION = 'dynamic-v3.3.4';
+const CACHE_FILES = [
+    '/css/style.min.css',
+    '/manifest.json',
+    '/js/main.min.js',
+    '/offline',
+    '/',
+    '/images/no-signal.png',
+    '/images/favicon.ico',
+    '/images/icon-192.png',
+    '/images/icon-256.png',
+    '/images/icon-384.png',
+    '/images/icon-512.png',
+];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(caches.open(CORE_CACHE_VERSION).then((cache) => cache.addAll(CACHE_FILES)));
